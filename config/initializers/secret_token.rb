75ec547793880410bfcb500b9cfdfa5ac2bc8761
Colin Casey <casey.colin@gmail.com>
2013-10-23 21:07:58 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SjdugWeb::Application.config.secret_key_base = '4e0a727e4e80f02dc4ba79bfbd9a061f46b8d33645bd27243c6d2f6c284b6a9551551d8bd3889b145a55b1500fa06c35544a3f6d8c23a11f34912240a77b2dcf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SjdugWeb::Application.config.session_store :cookie_store, key: '_sjdug-web_session'
