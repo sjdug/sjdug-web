4357d958cf056bfb62b3c51cacec8d9d5bca3c49
Chris Legault <chrislegault2011@gmail.com>
2013-10-26 18:21:52 -0300

    Removed un-necessary brackets

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,7 @@
 class ApiController < ApplicationController
   respond_to :json
 
-  def subscribe()
+  def subscribe
     mc = Mailchimp::API.new ENV['MAILCHIMP_API_KEY']
     render json: mc.lists().subscribe(ENV['MAILCHIMP_LIST_ID'], Struct.new(:email).new(params[:email]))
   end
